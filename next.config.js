bbef5481fe02f7006cf816a56e7b1dac7bf09030
Luís Rodrigues <luis.cnm@gmail.com>
2023-01-11 08:29:21 +0000

    Added profile links the the UI

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["luiro-blog.vercel.app"],
+    domains: ["luiro-blog.vercel.app", "avatars.githubusercontent.com"],
   },
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,39 @@
 import Image from "next/image";
 import data from "../data.json";
 
+function LinkCard({
+  href,
+  title,
+  image,
+}: {
+  href: string;
+  title: string;
+  image?: string;
+}) {
+  return (
+    <a
+      href={href}
+      className="flex items-center p-1 w-full rounded-md hover:scale-105 transition-all border border-gray-300 mb-3"
+    >
+      <div className="flex text-center w-full">
+        {image && (
+          <Image
+            className="rounded-sm"
+            alt={title}
+            src={image}
+            width={40}
+            height={40}
+          />
+        )}
+        <h2 className="font-semiboldb w-full text-center">{title}</h2>
+      </div>
+    </a>
+  );
+}
+
 export default function Home() {
   return (
-    <div className="flex items-center flex-col mx-auto w-full justify-center mt-16">
+    <div className="flex items-center flex-col mx-auto w-full justify-center mt-16 px-8">
       <Image
         className="rounded-full"
         alt={data.name}
@@ -11,7 +41,10 @@ export default function Home() {
         width={80}
         height={80}
       />
-      <h1 className="font-bold mt-4 text-xl">{data.name}</h1>
+      <h1 className="font-bold mt-4 mb-8 text-xl">{data.name}</h1>
+      {data.links.map((link) => (
+        <LinkCard key={link.href} {...link} />
+      ))}
     </div>
   );
 }
