07c8c487953fd127721b580e312b0fdf74d47b1e
Luís Rodrigues <luis.cnm@gmail.com>
2023-01-11 16:45:00 +0000

    Docs cleanup

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head({ params }: { params: { slug: string } }) {
   return (
     <>
-      <title>Luis' Linktree</title>
+      <title>Luís' Linktree</title>
       <meta name="description" content="Next.js + Tailwind CSS" />
       <link rel="icon" href="/favicon.ico" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
